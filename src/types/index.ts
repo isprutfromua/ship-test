cd8f13e3fe18ebbf20d354dcd95ca8cab0f6d5ac
Ivan Sprut <i.sprutfromua@gmail.com>
2024-01-08 11:46:40 +0200

    General types

---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+import {Resource, Texture} from "pixi.js";
+
+export type TPointTupple = [x: number, y: number];
+export type TWallSetting = [xPercent: number,yPercent: number, width: number, heightPercent: number]
+export enum EColor {Red = 'Red', Green = 'Green'}
+
+export type TTexturesMap = {
+    [key in Lowercase<EColor> | 'dock']: {
+        full: Texture<Resource>;
+        empty: Texture<Resource>;
+    };
+};
