301631d68dd93f68822235b991a8bde46d182bc4
Ivan Sprut <i.sprutfromua@gmail.com>
2024-01-08 11:45:48 +0200

    Helper functions added

---
+++ src/helpers/index.ts
@@ -0,0 +1,15 @@
+import {MAX_CAPACITY, MIN_CAPACITY, PIERS_COUNT} from "../settings.ts";
+import {Application} from "pixi.js";
+
+export function genPiers() {
+    const piers: number[] = []
+
+    for (let i = 0; i < PIERS_COUNT; i++) {
+        piers.push(Math.floor(Math.random() * (MAX_CAPACITY - MIN_CAPACITY + 1)) + MIN_CAPACITY)
+    }
+
+    return piers
+}
+export function percentsToCoords(app: Application, percents: [x: number, y: number]): [x: number, y: number] {
+    return [~~((app.view.width / 100) * percents[0]), ~~((app.view.height / 100) * percents[1])]
+}
